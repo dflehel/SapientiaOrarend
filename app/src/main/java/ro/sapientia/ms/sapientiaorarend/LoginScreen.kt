521a84093e373a85db661d0c2c2820bfb03f400c
csaba.marton <martoncs96@gmail.com>
2018-11-04 18:51:14 +0200

    Login screen develop3

--- app/src/main/java/ro/sapientia/ms/sapientiaorarend/LoginScreen.kt
+++ app/src/main/java/ro/sapientia/ms/sapientiaorarend/LoginScreen.kt
@@ -1,12 +1,31 @@
 package ro.sapientia.ms.sapientiaorarend
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginScreen : AppCompatActivity() {
+
+    lateinit var Email:EditText
+    lateinit var Phone:EditText
+    lateinit var Password:EditText
+    lateinit var Login:Button
+    lateinit var Signup:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+        this.Email = findViewById(R.id.login_screen_email)
+        this.Phone = findViewById(R.id.login_screen_phone)
+        this.Password = findViewById(R.id.login_screen_password)
+        this.Login = findViewById(R.id.login_screen_login_button)
+        this.Signup = findViewById(R.id.login_screen_signup_button)
+        this.Login.setOnClickListener{
+            var intent = Intent(this, MainScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
