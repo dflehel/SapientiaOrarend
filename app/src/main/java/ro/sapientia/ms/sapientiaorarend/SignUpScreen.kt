9261bb9d49c6c29789c4aba1ede57154e3cb6701
Dénes-Fazakas Lehel <lehelike@gmail.com>
2018-11-04 19:34:43 +0200

    Sigup Firebase

--- app/src/main/java/ro/sapientia/ms/sapientiaorarend/SignUpScreen.kt
+++ app/src/main/java/ro/sapientia/ms/sapientiaorarend/SignUpScreen.kt
@@ -1,10 +1,14 @@
 package ro.sapientia.ms.sapientiaorarend
 
+import android.app.ProgressDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpScreen : AppCompatActivity() {
 
@@ -18,17 +22,23 @@ class SignUpScreen : AppCompatActivity() {
     lateinit var edittextpassword:EditText
     lateinit var edittextpasswordconfirm:EditText
     lateinit var button: Button
+    lateinit var progressDialog: ProgressDialog
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_screen)
-
+        mAuth = FirebaseAuth.getInstance()
         this.editextname = findViewById<EditText>(R.id.sign_up_screen_name_editText)
         this.edittexphone = findViewById<EditText>(R.id.sign_up_screen_phone_editText)
         this.edittextemail = findViewById<EditText>(R.id.sign_up_screen_email_editText)
         this.edittextpassword = findViewById<EditText>(R.id.sign_up_screen_passwod_editText)
         this.edittextpasswordconfirm = findViewById<EditText>(R.id.sign_up_screen_configuration_password_textEdit)
         this.button = findViewById<Button>(R.id.sign_up_screen_sign_up_button)
+        this.progressDialog = ProgressDialog(this)
+        this.button.setOnClickListener{
+            this.registeruser()
+        }
     }
 
 
@@ -37,9 +47,39 @@ class SignUpScreen : AppCompatActivity() {
     }
 
     fun registeruser(){
-        this.name = this.editextname.toString().trim()
-        this.email = this.edittextemail.toString().trim()
-        this.password = this.edittextpassword.toString().trim()
-        if(TextUtils.isEmpty(email))
+        this.name = this.editextname.text.toString()
+        this.email = this.edittextemail.text.toString()
+        this.password = this.edittextpassword.text.toString()
+        this.progressDialog.setMessage("Regisztralas")
+        this.progressDialog.show()
+       if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Adjon Email cimmet",Toast.LENGTH_LONG).show()
+            return
+       }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Irjon be passwordot",Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if(this.edittextpassword.text.toString().equals(this.edittextpasswordconfirm.text.toString()) == false){
+            Toast.makeText(this,"Nem egyezik meg a jelszo",Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in: success
+                    // update UI for current User
+                    Toast.makeText(this,"Sikeres Registracio",Toast.LENGTH_LONG).show()
+                    val user = mAuth!!.currentUser
+                    //updateUI(user)
+                } else {
+                    // Sign in: fail
+                    // updateUI(null)
+                    Toast.makeText(this,"Sikeretelen Registracio",Toast.LENGTH_LONG).show()
+                }
+            }
+        this.progressDialog.dismiss()
     }
 }
