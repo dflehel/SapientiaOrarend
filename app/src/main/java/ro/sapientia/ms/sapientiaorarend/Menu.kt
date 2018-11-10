c4752e5f09a9aba21679a24a29411a23e612cd9b
csaba.marton <martoncs96@gmail.com>
2018-11-10 21:51:47 +0200

    Etkezde menu

---
+++ app/src/main/java/ro/sapientia/ms/sapientiaorarend/Menu.kt
@@ -0,0 +1,13 @@
+package ro.sapientia.ms.sapientiaorarend
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+    }
+}
