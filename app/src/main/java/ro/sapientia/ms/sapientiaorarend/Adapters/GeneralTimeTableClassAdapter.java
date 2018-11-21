ad21f6fe32adc6ec61f9667abde956e27a2c01e8
Dénes-Fazakas Lehel <lehelike@gmail.com>
2018-11-21 16:45:08 +0200

    colored class adapted

--- app/src/main/java/ro/sapientia/ms/sapientiaorarend/Adapters/GeneralTimeTableClassAdapter.java
+++ app/src/main/java/ro/sapientia/ms/sapientiaorarend/Adapters/GeneralTimeTableClassAdapter.java
@@ -1,12 +1,14 @@
 package ro.sapientia.ms.sapientiaorarend.Adapters;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import ro.sapientia.ms.sapientiaorarend.R;
+import ro.sapientia.ms.sapientiaorarend.models.ClassColorsBuilder;
 import ro.sapientia.ms.sapientiaorarend.models.Classes;
 
 import java.util.ArrayList;
@@ -41,6 +43,10 @@ public class GeneralTimeTableClassAdapter  extends  RecyclerView.Adapter< Genera
         ownTimeTableViewClassHolder.classroom.setText(this.c.get(i).getClassroom());
         ownTimeTableViewClassHolder.startt.setText(this.c.get(i).getStart().toString());
         ownTimeTableViewClassHolder.endt.setText(this.c.get(i).getEnd().toString());
+        if (ClassColorsBuilder.colors.get(this.c.get(i).getTeacher()) != null) {
+            ownTimeTableViewClassHolder.cardView.setCardBackgroundColor(ClassColorsBuilder.colors.get(this.c.get(i).getTeacher()));
+
+        }
     }
 
     @Override
@@ -54,15 +60,18 @@ public class GeneralTimeTableClassAdapter  extends  RecyclerView.Adapter< Genera
         public TextView material;
         public TextView startt;
         public TextView endt;
+        public CardView cardView;
 
 
         public OwnTimeTableViewClassHolder(@NonNull View itemView) {
             super(itemView);
+            this.cardView = (CardView) itemView.findViewById(R.id.class_view);
             this.teacher = (TextView) itemView.findViewById(R.id.teacher_name);
             this.classroom = (TextView) itemView.findViewById(R.id.class_number);
             this.material =(TextView) itemView.findViewById(R.id.class_name);
             this.startt = (TextView) itemView.findViewById(R.id.start_time);
             this.endt = (TextView) itemView.findViewById(R.id.end_time);
+
         }
     }
 }
--- app/src/main/java/ro/sapientia/ms/sapientiaorarend/models/ClassPathBuilder.java
+++ app/src/main/java/ro/sapientia/ms/sapientiaorarend/models/ClassPathBuilder.java
@@ -42,7 +42,6 @@ public class ClassPathBuilder {
                             }
                         }
                     }
-                  System.out.println(ClassPathBuilder.this.toString());
             }
 
             @Override
